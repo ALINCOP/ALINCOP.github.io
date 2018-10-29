4d0bb3f1af41e74b0eb1562d5f94e0e080f52282
Alin Copindean <alin.copindean@yahoo.com>
2018-10-29 15:32:26 +0200

    Update html5/orientation.js

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.10.29.1";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.1";
 window.addEventListener("deviceorientation", on_device_orientation);
 //--------------------------------
 function on_device_orientation(e)
