a26d3a76734cf1768f2fbebefe52af7785c9034a
Alin Copindean <alin.copindean@yahoo.com>
2018-10-29 15:33:43 +0200

    Update html5/orientation.js

--- html5/orientation.js
+++ html5/orientation.js
@@ -3,7 +3,7 @@ window.addEventListener("deviceorientation", on_device_orientation);
 //--------------------------------
 function on_device_orientation(e)
 {
-	document.getElementById("id_alhpa").innerHTML=e.alpha;
+	document.getElementById("id_alpha").innerHTML=e.alpha;
 	document.getElementById("id_beta").innerHTML=e.beta;
 	document.getElementById("id_gamma").innerHTML=e.gamma;
 }
